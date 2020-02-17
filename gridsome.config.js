18872224d753d4528c2d66969bb624aef51674d6
Joe Schr <joe.vandelay@gmail.com>
2020-02-17 23:12:09 +0100

    kind of working mix of github copies

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,15 @@
-// This is where project configuration and plugin options are located. 
-// Learn more: https://gridsome.org/docs/config
-
-// Changes here require a server restart.
-// To restart press CTRL + C in terminal and run `gridsome develop`
+const path = require(`path`);
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: `Building a Static Site with Storyblok and Gridsome`,
+  plugins: [
+    {
+      use: 'gridsome-source-storyblok',
+      options: {
+        client: {
+        accessToken: `Uy6KQR3Xtv5mi9jGi4vozwtt`,
+        }
+      }
+    }
+  ],
+};
