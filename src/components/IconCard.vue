f71a485e71815ed6d568c2d82652a7fa4d9a79fa
Joe Schr <joe.vandelay@gmail.com>
2020-09-28 20:41:16 +0200

    add image card

---
+++ src/components/IconCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-white border rounded-lg overflow-hidden">
+    <g-image :alt="item.imageAlt" :src="item.imageUrl">
+      <div class="p-6">
+        <div class="flex items-baseline">
+          <span
+            class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+            >{{ item.title }}
+          </span>
+        </div>
+        <!-- <h4 class="mt-1 font-semibold text-lg leading-tight truncate">
+        {{ item.title }}
+      </h4> -->
+        <div class="mt-2 flex items-center">
+          <span class="ml-2 text-gray-600 text-sm">{{ item.content }} </span>
+        </div>
+      </div>
+    </g-image>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,39 +1,200 @@
 <template>
   <Layout>
-    <p>
-      <button class="btn btn-brand-yellow">Anmelden</button>
-    </p>
-    <p>
-      Unsere Deutschkurse könnt ihr ab sofort Online besuchen. Trefft Elke,
-      Laura & Kolleg_innen live in den virtuellen Work IT AUT Klassenzimmern.
-    </p>
-    <h4>CHECK IT AUT</h4>
-    <p>
-      Die Lebens- und Arbeitswelten sind in den letzten Monaten durcheinander
-      geraten. Wir haben unser Projekt erweitert und freuen uns euch ab sofort
-      unsere DAF/DAZ Kurse ONLINE & LIVE anbieten zu können.
-    </p>
-    <h4>ALLGEMEINE INFOS</h4>
-    <p>
-      Deutschkurse / Die Inhalte & Lehrunterlagen folgen den Vorgaben des
-      Gemeinsamen Europäi- schen Referenzrahmen für Sprachen. Trainer_innen
-      verfügen über DAF/DAZ Zertifizierung und /oder ÖIF (österreichischer
-      Integrationsfond) Zertifizierung. Italienischkurse / Diese Kurse finden in
-      Kooperation mit der One World Cagliari statt. www.oneworldcagliari.com
-    </p>
+    <div class="grid grid-cols-2 gap-4 h-full">
+      <div>
+        <p>
+          <button class="btn btn-brand-yellow">Anmelden</button>
+        </p>
+        <p>
+          Unsere Deutschkurse könnt ihr ab sofort Online besuchen. Trefft Elke,
+          Laura & Kolleg_innen live in den virtuellen Work IT AUT
+          Klassenzimmern.
+        </p>
+        <h4>CHECK IT AUT</h4>
+        <p>
+          Die Lebens- und Arbeitswelten sind in den letzten Monaten
+          durcheinander geraten. Wir haben unser Projekt erweitert und freuen
+          uns euch ab sofort unsere DAF/DAZ Kurse ONLINE & LIVE anbieten zu
+          können.
+        </p>
+        <h4>ALLGEMEINE INFOS</h4>
+        <p>
+          Deutschkurse / Die Inhalte & Lehrunterlagen folgen den Vorgaben des
+          Gemeinsamen Europäi- schen Referenzrahmen für Sprachen. Trainer_innen
+          verfügen über DAF/DAZ Zertifizierung und /oder ÖIF (österreichischer
+          Integrationsfond) Zertifizierung. Italienischkurse / Diese Kurse
+          finden in Kooperation mit der One World Cagliari statt.
+          www.oneworldcagliari.com
+        </p>
+      </div>
+      <div class="grid grid-cols-3 gap-4">
+        <div class="bg-white border rounded-lg overflow-hidden">
+          <g-image alt="Deutschkurse" src="~/assets/1_Deutschkurse.jpg" />
+          <div class="p-6">
+            <div class="flex items-baseline">
+              <span
+                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+                >Deutschkurse
+              </span>
+            </div>
+            <div class="mt-2 flex items-center">
+              <span class="ml-2 text-gray-600 text-sm"
+                >Standardkurse, Berufliches Deutsch
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white border rounded-lg overflow-hidden">
+          <g-image
+            alt="Italienischkurs"
+            src="~/assets/2_Italienischkurs_Gallura_home_o.jpg"
+          />
+          <div class="p-6">
+            <div class="flex items-baseline">
+              <span
+                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+                >Italienischkurs
+              </span>
+            </div>
+            <div class="mt-2 flex items-center">
+              <span class="ml-2 text-gray-600 text-sm"
+                >Carbonara è senza crema!
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white border rounded-lg overflow-hidden">
+          <g-image
+            alt="Kursprogramm"
+            src="~/assets/3_Kursprogramm_Sprachraum_1_iStock-1214385234.jpg"
+          />
+          <div class="p-6">
+            <div class="flex items-baseline">
+              <span
+                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+                >Kursprogramm</span
+              >
+            </div>
+            <div class="mt-2 flex items-center">
+              <span class="ml-2 text-gray-600 text-sm"
+                >Kurse, Termine und Preise Alles auf einen Blick!
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white border rounded-lg overflow-hidden">
+          <g-image alt="Team" src="~/assets/LogoOriginal.jpg" />
+          <div class="p-6">
+            <div class="flex items-baseline">
+              <span
+                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+                >Team
+              </span>
+            </div>
+            <div class="mt-2 flex items-center">
+              <span class="ml-2 text-gray-600 text-sm"
+                >Standardkurse, Berufliches Deutsch
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white border rounded-lg overflow-hidden">
+          <g-image alt="Einstufungstest" src="~/assets/LogoOriginal.jpg" />
+          <div class="p-6">
+            <div class="flex items-baseline">
+              <span
+                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+                >Einstufungstest
+              </span>
+            </div>
+            <div class="mt-2 flex items-center">
+              <span class="ml-2 text-gray-600 text-sm"
+                >Überprüfe dein Niveau!
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white border rounded-lg overflow-hidden">
+          <g-image alt="Firmenkurse" src="~/assets/LogoOriginal.jpg" />
+          <div class="p-6">
+            <div class="flex items-baseline">
+              <span
+                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+                >Firmenkurse
+              </span>
+            </div>
+            <div class="mt-2 flex items-center">
+              <span class="ml-2 text-gray-600 text-sm"
+                >Individuelles Programm Gesundheitsberufe Hotellerieberufe
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white border rounded-lg overflow-hidden">
+          <g-image
+            alt="Leben &amp; Arbeiten"
+            src="~/assets/8_LebenArbeiten_ReizendKaroVisit.jpg"
+          />
+          <div class="p-6">
+            <div class="flex items-baseline">
+              <span
+                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+                >Leben &amp; Arbeiten
+              </span>
+            </div>
+            <div class="mt-2 flex items-center">
+              <span class="ml-2 text-gray-600 text-sm"
+                >Übersetzungen, Bewerbungsunterlagen
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="bg-white border rounded-lg overflow-hidden">
+          <g-image
+            alt="Jobs in Österreich"
+            src="~/assets/9_50257207_282289765784856_3876881521411358720_o.jpg"
+          />
+          <div class="p-6">
+            <div class="flex items-baseline">
+              <span
+                class="inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide"
+                >Work IT AUT
+              </span>
+            </div>
+            <div class="mt-2 flex items-center">
+              <span class="ml-2 text-gray-600 text-sm"
+                >Jobs in Österreich
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 </template>
 
 <script>
+import IconCard from '~/components/IconCard.vue'
 import Layout from '~/layouts/Default.vue'
 
 export default {
   components: {
+    IconCard,
     Layout,
   },
   metaInfo: {
     title: 'Home',
   },
+  data: () => ({
+    cards: [
+      {
+        imageUrl: '/assets/1_Deutschkurse.jpg',
+        imageAlt: 'Deutschkurse',
+        title: 'Deutschkurse',
+        content: `A1–C1`,
+      },
+    ],
+  }),
 }
 </script>
 
