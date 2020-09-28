75bca426cfa5f34ddfce31f34a610a209905b08c
Joe Schr <joe.vandelay@gmail.com>
2020-09-28 19:48:54 +0200

    start text and style

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,31 @@
 <template>
   <Layout>
-    <h4>Hello, world!</h4>
-
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-      excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
-      Dolores, aperiam non officia eos quod asperiores
+      <button class="btn btn-brand-yellow">Anmelden</button>
+    </p>
+    <p>
+      Unsere Deutschkurse könnt ihr ab sofort Online besuchen. Trefft Elke,
+      Laura & Kolleg_innen live in den virtuellen Work IT AUT Klassenzimmern.
+    </p>
+    <h4>CHECK IT AUT</h4>
+    <p>
+      Die Lebens- und Arbeitswelten sind in den letzten Monaten durcheinander
+      geraten. Wir haben unser Projekt erweitert und freuen uns euch ab sofort
+      unsere DAF/DAZ Kurse ONLINE & LIVE anbieten zu können.
+    </p>
+    <h4>ALLGEMEINE INFOS</h4>
+    <p>
+      Deutschkurse / Die Inhalte & Lehrunterlagen folgen den Vorgaben des
+      Gemeinsamen Europäi- schen Referenzrahmen für Sprachen. Trainer_innen
+      verfügen über DAF/DAZ Zertifizierung und /oder ÖIF (österreichischer
+      Integrationsfond) Zertifizierung. Italienischkurse / Diese Kurse finden in
+      Kooperation mit der One World Cagliari statt. www.oneworldcagliari.com
     </p>
   </Layout>
 </template>
 
 <script>
-import Layout from '~/layouts/Default.vue';
+import Layout from '~/layouts/Default.vue'
 
 export default {
   components: {
@@ -20,7 +34,18 @@ export default {
   metaInfo: {
     title: 'Home',
   },
-};
+}
 </script>
 
-<style></style>
+<style>
+p {
+  padding-top: 0.5em;
+  padding-bottom: 2em;
+  line-height: 1.5em;
+  font-weight: 500;
+}
+h4 {
+  color: #ffffff;
+  line-height: 2em;
+}
+</style>
