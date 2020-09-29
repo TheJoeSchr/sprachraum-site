eab414ff4429494bec6ba3871e5c7677d57f5a6e
Joe Schr <joe.vandelay@gmail.com>
2020-09-29 20:27:29 +0200

    add custom responsive design

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,43 +1,47 @@
 <template lang="pug">
 .bg-body.text-body.font-body
   .container.mx-auto.px-4
-  div(style='max-width: 1465px; width: 1465px')
-    //  Top Bar
-    .z-20.fixed.bg-brand-yellow.w-full(style='height: 265px; width: 1465px')
-      .absolute.top-0.left-0.bg-white
-        g-image(alt='Logo' src='~/assets/LogoOriginalDe.png' width='265').
+    //- HEADER-LOGO
+    g-image(src="~/assets/LogoAlphaFull.png" alt="Lass uns reden, Schatz!")
+    //- HEADER
+    nav(class="flex flex-wrap items-center justify-between p-4")
+        div(class="flex flex-shrink-0 mr-6")
+            a(href="#" class="text-xl text-black font-semibold") Lass uns reden, Schatz!
+        div(class="block lg:hidden")
+            <button class="navbar-burger flex items-center py-2 px-3 text-primary-500 rounded border border-indigo-500"><svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path></svg></button>
+        div(class="navbar-menu hidden lg:flex lg:flex-grow lg:items-center w-full lg:w-auto")
+            div(class="lg:ml-auto")
+                a(href="#" class="block lg:inline-block mt-4 lg:mt-0 mr-10 text-secondary-900 hover:text-secondary-700") Deutschkurse
+                a(href="#" class="block lg:inline-block mt-4 lg:mt-0 mr-10 text-secondary-900 hover:text-secondary-700") Italienschkurse
+                a(href="#" class="block lg:inline-block mt-4 lg:mt-0 mr-10 text-secondary-900 hover:text-secondary-700") Team
+            div
+                a(href="#" class="inline-block py-3 px-5 mr-4 leading-none text-center text-gray-700 bg-primary-100 hover:bg-primary-200 rounded shadow") Kursprogramm
+                a(href="#" class="inline-block py-3 px-6 mt-4 lg:mt-0 leading-none text-black bg-primary-500 hover:bg-primary-600 rounded shadow") Anmelden
+    //- CONTENT
+    slot
+    //- FOOTER
+    footer
+        div(class="flex flex-wrap items-center py-4 px-4 border-b")
+            div(class="w-full lg:w-1/5 text-center lg:text-left")
+                a(class="text-xl text-primary-500 font-semibold" href="#")
+                    g-image(src="~/assets/LogoAlphaFull.png" alt="SPRACHRAUM.workitaut.at" width="100")
 
+            div(class="w-full lg:w-4/5 mt-4 lg:mt-0 text-center")
+                a SPRACHRAUM.workitaut.at
+                a(class="inline-block mx-4 mb-4 lg:mb-0 text-secondary-900 hover:text-secondary-700" href="#") Sprachkurse
+                a(class="inline-block mx-4 mb-4 lg:mb-0 text-secondary-900 hover:text-secondary-700" href="#") Firmenkurse
+                a(class="inline-block mx-4 mb-4 lg:mb-0 text-secondary-900 hover:text-secondary-700" href="#") Kontakt
+                a(class="inline-block mx-4 mb-4 lg:mb-0 text-secondary-900 hover:text-secondary-700" href="#") Work IT AUT
 
-      .absolute.top-0.bg-white(style='left: 265px')
-        g-image(alt='Banner' src='~/assets/BannerOriginal.jpg' width='1200' style='height: 265px; width: 1200px').
+            //- div(class="flex justify-center lg:justify-end w-full lg:w-1/5 my-2 lg:my-0")
+            //-     img(src="placeholders/icons/message.svg" alt="" class="w-6 h-6" )
+            //-     img(src="placeholders/icons/share.svg" alt="" class="w-6 h-6 mx-8" )
+            //-     img(src="placeholders/icons/star.svg" alt="" class="w-6 h-6" )
 
-
-    .absolute.bg-brand-green.h-full(style='margin-top: 265px')
-      //  Sidebar Bar
-      #sidebar.z-10.fixed.bg-brand-yellow.h-full.pt-1(style='width: 265px')
-        ul.container.mx-auto.text-center.pt-6
-          li
-            a.text-black(class="hover:text-white" href='#') DEUTSCH
-          li
-            a.text-black(class="hover:text-white" href='#') ITALIENISCH
-          li
-            a.text-black(class="hover:text-white" href='#') KURSPROGRAMM
-          li
-            a.text-black(class="hover:text-white" href='#') TEAM
-          li
-            a.text-black(class="hover:text-white" href='#') EINSTUFUNGSTEST
-          li
-            a.text-black(class="hover:text-white" href='#') FIRMENKURSE
-          li
-            a.text-black(class="hover:text-white" href='#') LEBEN & ARBEITEN
-          li
-            a.text-black(class="hover:text-white" href='#') WORK IT AUT
-      // Main Content
-      #content.z-0.h-full.pt-1(style='padding-left: 265px; width: 1465px; overflow-y: auto')
-        .bg-brand-green.px-6
-          slot.
-
-
+        div(class="py-4 text-center")
+            span(class="block md:inline-block mb-4 md:mb-0 mx-3") © 2020 sprachraum.workitaut.at
+            a(class="inline-block mx-4 text-secondary-900 hover:text-secondary-700" href="#") Impressum
+            a(class="inline-block mx-4 text-secondary-900 hover:text-secondary-700" href="#") AGB
 </template>
 
 <static-query>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,34 +1,121 @@
-<template>
-  <Layout>
-    <div class="grid grid-cols-2 gap-4 h-full">
-      <div>
-        <p>
-          <button class="btn btn-primary">Anmelden</button>
-        </p>
-        <p>
-          Unsere Deutschkurse könnt ihr ab sofort Online besuchen. Trefft Elke,
-          Laura & Kolleg_innen live in den virtuellen Work IT AUT
-          Klassenzimmern.
-        </p>
-        <h4>CHECK IT AUT</h4>
-        <p>
-          Die Lebens- und Arbeitswelten sind in den letzten Monaten
-          durcheinander geraten. Wir haben unser Projekt erweitert und freuen
-          uns euch ab sofort unsere DAF/DAZ Kurse ONLINE & LIVE anbieten zu
-          können.
-        </p>
-        <h4>ALLGEMEINE INFOS</h4>
-        <p>
-          Deutschkurse / Die Inhalte & Lehrunterlagen folgen den Vorgaben des
-          Gemeinsamen Europäi- schen Referenzrahmen für Sprachen. Trainer_innen
-          verfügen über DAF/DAZ Zertifizierung und /oder ÖIF (österreichischer
-          Integrationsfond) Zertifizierung. Italienischkurse / Diese Kurse
-          finden in Kooperation mit der One World Cagliari statt.
-          www.oneworldcagliari.com
-        </p>
-      </div>
-    </div>
-  </Layout>
+<template lang="pug">
+Layout
+  section(class="py-12 px-4 text-center")
+      div(class="w-full max-w-2xl mx-auto")
+          span(class="text-sm bg-primary-500 font-semibold")
+              | ++ SPRECHEN ++ SCHREIBEN ++ H&Ouml;REN ++ LESEN ++ PARLARE ++ SCRIVERE ++ ASCOLTARE ++ LEGGERE ++
+          h2(class="text-5xl mt-2 mb-6 leading-tight font-heading")
+              | Deutsch &amp; Italienisch
+              br
+              |  Kurse &amp; Coaching
+              br
+              |  Online &amp; Live
+          a(class="text-secondary-700 hover:underline", href="#")
+              | Zertifizierte DAF/DAZ Trainer*innen. Gemeinsamer europ&auml;ischer Referenzrahmen f&uuml;r Sprachen.
+
+
+  section(class="py-12 px-4")
+      div(class="flex flex-wrap items-center text-center lg:text-left -mx-2")
+          div(class="lg:w-1/2 px-2 lg:pr-10 mt-10 lg:mt-0 order-1 lg:order-none")
+              h2(class="text-5xl mb-6 leading-tight font-heading") Unsere Deutschkurse könnt ihr ab sofort Online besuchen
+              p(class="mb-8 text-gray-500 leading-relaxed") Trefft Elke, Laura &amp; Kolleg_innen live in den virtuellen Work IT AUT Klassenzimmern.
+
+              div
+                  a(href="#" class="inline-block py-4 px-8 mr-6 leading-none text-black bg-primary-500 hover:bg-primary-600 rounded shadow") Gleich anmelden!
+                  a(href="#" class="text-secondary-700 hover:underline") >> Einstufungstest
+
+          div(class="lg:w-1/2 px-2")
+              img(src="images/SPRACHRAUM-GER-FINAL-NEU.png" alt="")
+
+  section(class="py-12 px-4")
+      div(class="flex flex-wrap items-center max-w-5xl mx-auto")
+          div(class="lg:w-1/2 px-8 mb-8 lg:mb-0")
+              img(src="images/LaptopQuaderKlein.jpg" alt="" class="lg:max-w-sm mx-auto")
+
+          div(class="lg:w-1/2 px-8 mb-8 lg:mb-0")
+              h3(class="text-2xl mb-2 font-heading") Check IT AUT
+              p(class="mb-4 text-gray-500 leading-relaxed") Die Lebens- und Arbeitswelten sind in den letzten Monaten durcheinander geraten. Wir haben unser Projekt erweitert und freuen uns euch ab sofort unsere DAF/DAZ Kurse ONLINE &amp; LIVE anbieten zu können
+              a(href="#" class="text-secondary-700 hover:underline") >> Kursprogramm
+
+          div(class="lg:w-1/2 px-8 mb-8 lg:mb-0 lg:order-1")
+              img(src="images/7_Firmenkurse_iStock-620402800.jpg" alt="" class="lg:max-w-sm mx-auto")
+
+          div(class="lg:w-1/2 px-8")
+              h3(class="text-2xl mb-2 font-heading") Deutschkurse
+              p(class="mb-4 text-gray-500 leading-relaxed") Die Inhalte &amp; Lehrunterlagen folgen den Vorgaben des Gemeinsamen Europäi- schen Referenzrahmen für Sprachen. Trainer_innen verfügen über DAF/DAZ Zertifizierung und /oder ÖIF (österreichischer Integrationsfond) Zertifizierung. Italienischkurse / Diese Kurse finden in Kooperation mit der One World Cagliari statt. www.oneworldcagliari.com
+              a(href="#" class="text-secondary-700 hover:underline") >> Deutschkurse
+
+  section(class="py-12 px-4 text-center")
+      h2(class="text-4xl mb-2 leading-tight font-heading") Angebote
+      p(class="max-w-xl mx-auto mb-8 text-gray-500") Deutsch &amp; Italienisch Kurse &amp; Coaching Online &amp; Live  Zertifizierte DAF/DAZ Trainer*innen. Gemeinsamer europäischer Referenzrahmen für Sprachen.
+
+      div(class="flex flex-wrap justify-center max-w-4xl mx-auto")
+          div(class="h-48 w-48 md:h-64 md:w-64 m-2")
+              div(class="relative flex items-center h-full p-8 bg-cover rounded shadow-md" style="background-image: url('images/1_Deutschkurs_blicksalzburg-1133549562.jpg')")
+                  div(class="absolute inset-0 bg-white opacity-75 rounded")
+                  div(class="z-10 w-full text-center")
+                      p(class="text-3xl leading-tight mb-2") Deutschkurse
+                      p Standardkurse A1 – C1 Deutsch für den Beruf Konversation Individual Coaching
+                      div(class="flex justify-center mt-4")
+                          img(src="placeholders/icons/message.svg" alt="" class="w-8 h-8 mx-2" )
+                          img(src="placeholders/icons/share.svg" alt="" class="w-8 h-8 mx-2" )
+                          img(src="placeholders/icons/star.svg" alt="" class="w-8 h-8 mx-2" )
+
+          div(class="h-48 w-48 md:h-64 md:w-64 m-2")
+              div(class="relative flex items-center h-full p-8 bg-cover rounded shadow-md" style="background-image: url('placeholders/pictures/female_avatar.svg')" )
+                  div(class="hidden absolute inset-0 bg-white opacity-50 rounded")
+                  div(class="hidden z-10 w-full text-center")
+                      p(class="text-3xl leading-tight mb-2") Michael Scott
+                      p Regional Manager
+                      div(class="flex justify-center mt-4")
+                          img(src="placeholders/icons/message.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/share.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/star.svg" alt="" class="w-8 h-8 mx-2")
+
+          div(class="h-48 w-48 md:h-64 md:w-64 m-2")
+              div(class="relative flex items-center h-full p-8 bg-cover rounded shadow-md" style="background-image: url('placeholders/pictures/male_avatar.svg')" )
+                  div(class="hidden absolute inset-0 bg-white opacity-50 rounded")
+                  div(class="hidden z-10 w-full text-center")
+                      p(class="text-3xl leading-tight mb-2") Michael Scott
+                      p Regional Manager
+                      div(class="flex justify-center mt-4")
+                          img(src="placeholders/icons/message.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/share.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/star.svg" alt="" class="w-8 h-8 mx-2")
+
+          div(class="h-48 w-48 md:h-64 md:w-64 m-2")
+              div(class="relative flex items-center h-full p-8 bg-cover rounded shadow-md" style="background-image: url('placeholders/pictures/female_avatar.svg')" )
+                  div(class="hidden absolute inset-0 bg-white opacity-50 rounded")
+                  div(class="hidden z-10 w-full text-center")
+                      p(class="text-3xl leading-tight mb-2") Michael Scott
+                      p Regional Manager
+                      div(class="flex justify-center mt-4")
+                          img(src="placeholders/icons/message.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/share.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/star.svg" alt="" class="w-8 h-8 mx-2")
+
+          div(class="h-48 w-48 md:h-64 md:w-64 m-2")
+              div(class="relative flex items-center h-full p-8 bg-cover rounded shadow-md" style="background-image: url('placeholders/pictures/male_avatar.svg')" )
+                  div(class="hidden absolute inset-0 bg-white opacity-50 rounded")
+                  div(class="hidden z-10 w-full text-center")
+                      p(class="text-3xl leading-tight mb-2") Michael Scott
+                      p Regional Manager
+                      div(class="flex justify-center mt-4")
+                          img(src="placeholders/icons/message.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/share.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/star.svg" alt="" class="w-8 h-8 mx-2")
+
+          div(class="h-48 w-48 md:h-64 md:w-64 m-2")
+              div(class="relative flex items-center h-full p-8 bg-cover rounded shadow-md" style="background-image: url('placeholders/pictures/female_avatar.svg')" )
+                  div(class="hidden absolute inset-0 bg-white opacity-50 rounded")
+                  div(class="hidden z-10 w-full text-center")
+                      p(class="text-3xl leading-tight mb-2") Michael Scott
+                      p Regional Manager
+                      div(class="flex justify-center mt-4")
+                          img(src="placeholders/icons/message.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/share.svg" alt="" class="w-8 h-8 mx-2")
+                          img(src="placeholders/icons/star.svg" alt="" class="w-8 h-8 mx-2")
+
 </template>
 
 <script>
