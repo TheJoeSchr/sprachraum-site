35a5810308569da984877f82a09a230e633de3a2
Joe Schr <joe.vandelay@gmail.com>
2020-09-29 17:15:23 +0200

    adds postcss-purgecss to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 const plugin = require('tailwindcss/plugin')
+const tailwind = require('tailwindcss')
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+const postcssPlugins = [tailwind()]
+
+if (process.env.NODE_ENV === 'production')
+  postcssPlugins.push(purgecss(require('./purgecss.config.js')))
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
