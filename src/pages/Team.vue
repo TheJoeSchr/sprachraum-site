fe4dbdb62ee4c78df686761e9a09ee7cbc0e9ab8
Joe Schr <joe.vandelay@gmail.com>
2020-09-30 20:45:51 +0200

    add team page

---
+++ src/pages/Team.vue
@@ -0,0 +1,92 @@
+<template>
+  <section class="py-12 px-4">
+    <div class="flex flex-wrap -mx-4 mb-8">
+      <div class="lg:w-1/2 px-4 mb-4 lg:mb-0">
+        <img
+          class="rounded shadow-md"
+          src="placeholders/pictures/male_avatar.svg"
+          alt=""
+        />
+      </div>
+      <div class="lg:w-1/2 px-4">
+        <h2 class="text-4xl font-heading font-semibold">Michael Scott</h2>
+        <span class="text-xl">Regional Manager</span>
+        <p class="my-5">
+          Former branch manager of the Scranton branch of Dunder Mifflin Paper
+          Company, The Michael Scott Paper Company, and later Dunder Mifflin
+          Sabre. Michael enjoys writing song parodies.
+        </p>
+        <p class="my-5">
+          Michael was born on March 15, 1962, at 11:23 AM, in Scranton,
+          Pennsylvania. He was raised by his mother and stepfather, Jefferson
+          (“Jeff”), with a home on Kenneth Road.
+        </p>
+        <div class="flex">
+          <img
+            class="w-8 mr-6"
+            src="placeholders/icons/message.svg"
+            alt=""
+          /><img
+            class="w-8 mr-6"
+            src="placeholders/icons/share.svg"
+            alt=""
+          /><img class="w-8 mr-6" src="placeholders/icons/star.svg" alt="" />
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-wrap -mx-4">
+      <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
+        <img
+          class="rounded shadow-md"
+          src="placeholders/pictures/female_avatar.svg"
+          alt=""
+        />
+      </div>
+      <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
+        <img
+          class="rounded shadow-md"
+          src="placeholders/pictures/male_avatar.svg"
+          alt=""
+        />
+      </div>
+      <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
+        <img
+          class="rounded shadow-md"
+          src="placeholders/pictures/female_avatar.svg"
+          alt=""
+        />
+      </div>
+      <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
+        <img
+          class="rounded shadow-md"
+          src="placeholders/pictures/male_avatar.svg"
+          alt=""
+        />
+      </div>
+      <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
+        <img
+          class="rounded shadow-md"
+          src="placeholders/pictures/female_avatar.svg"
+          alt=""
+        />
+      </div>
+      <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
+        <img
+          class="rounded shadow-md"
+          src="placeholders/pictures/male_avatar.svg"
+          alt=""
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Team',
+  },
+}
+</script>
+
+<style></style>
