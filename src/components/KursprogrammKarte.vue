4d81048cd9d091a1cff87419695983efbf4ec5c8
Joe Schr <joe.vandelay@gmail.com>
2020-09-30 20:46:13 +0200

    add kursprogramm karte markup

---
+++ src/components/KursprogrammKarte.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="py-8">
+    <div class="flex flex-wrap max-w-4xl mx-auto">
+      <div class="w-full md:w-full p-4 mb-2 md:mb-0">
+        <div class="py-3 px-6 border">
+          <img class="ml-auto" src="placeholders/icons/badge.svg" alt="" />
+          <h3 class="text-5xl mb-3 font-heading font-semibold left-0">
+            $9 / mo
+          </h3>
+          <div class="mx-0 md:mx-32">
+            <p class="mb-8 mx-0 text-gray-500 leading-relaxed">
+              Just enough paper for small business. Not enough to waste it on
+              fluffy animals photos.
+            </p>
+            <h3 class="mb-3 font-heading font-semibold">Includes</h3>
+            <ul class="mb-8 list-inside list-disc text-gray-500">
+              <li class="mb-1">10 reams of paper</li>
+              <li class="mb-1">5$ delivery service</li>
+              <li class="mb-1">20% discount on the other plans</li>
+              <li class="mb-1">Free help desk consultancy</li>
+            </ul>
+          </div>
+
+          <div class="ml-auto">
+            <a
+              class="inline-block w-full py-3 px-5 leading-none text-center text-blue-700 bg-indigo-100 hover:bg-indigo-200 rounded shadow"
+              href="#"
+              >Contact sales</a
+            >
+            <p class="text-sm text-center my-3">* Price includes VAT.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Team',
+  },
+}
+</script>
+
+<style></style>
