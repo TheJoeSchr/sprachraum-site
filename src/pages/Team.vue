f560e508a458792cadf7a99feba70bf42ddf9f2f
Joe Schr <joe.vandelay@gmail.com>
2020-10-01 17:52:25 +0200

    team: pimp & img are quaders now

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -3,26 +3,26 @@ Layout
   .divide-y.divide-gray-400
     section.py-12.px-4.container.py-2(v-for='(person,index) in team' :key='person.id').pt-8
       .flex.flex-wrap.-mx-4.mb-8
-        .portrait.mb-4.mx-auto(class="lg:ml-0 lg:w-1/2 lg:mb-0 h-32" :class="person.id")
-        .w-full.px-4(class='lg:w-1/2')
+        .portrait.mx-auto(class="md:mx-0 md:w-1/3 md:mb-0 md:pr-8" :class="person.id")
+        .w-full.px-4(class='lg:w-1/2 ')
           h2.text-4xl.font-heading.font-semibold.text-center.mx-auto(class="lg:text-left").
             {{ person.name }}
           .py-8.px-4(class="lg:pl-0")
             .max-w-2xl.pl-12.py-4.mx-auto.border-l-4(class="lg:pl-8")
               p.text-xl.mb-6 {{ person.zitat }}
-              p.text-sm
-                span.mr-2  {{ person.zitatAuthor}}
+              p.text-sm.text-gray-600
+                span.mr-2  - {{ person.zitatAuthor}}
                 //- span.font-semibold CEO at Realweb
       .flex.flex-wrap.-mx-4.w-full
-        .shorty
-          h3.text-xl Sprachraum
-          p.tx-sm(v-html="toBr(person.sprachraum)")
-        .shorty
+        .shorty.-mx-4
           h3.text-xl Kurzbio
-          p.tx-sm(v-html="toBr(person.kurzbio)")
-        .shorty
+          p.tx-sm.text-gray-700(v-html="toBr(person.kurzbio)")
+        .shorty.mx-4
+          h3.text-xl Sprachraum
+          p.tx-sm.text-gray-700(v-html="toBr(person.sprachraum)")
+        .shortymx-4
           h3.text-xl Schlagworte
-          p.tx-sm(v-html="schlagworte(person)")
+          p.tx-sm.text-gray-700(v-html="schlagworte(person)")
 </template>
 <style scoped>
 .shorty {
@@ -41,22 +41,22 @@ Layout
   background-position: center center; /* optional, center the image */
 }
 .elke {
-  background-image: url('~@/assets/portraits/elke.jpg?width=300&height=300');
+  background-image: url('~@/assets/portraits/Elke.jpg?width=300&height=300');
 }
 .johannes {
-  background-image: url('~@/assets/portraits/johannes.jpg?width=300&height=300');
+  background-image: url('~@/assets/portraits/Johannes.jpg?width=300&height=300');
 }
 .heidi {
-  background-image: url('~@/assets/portraits/heidi.jpg?width=300&height=300');
+  background-image: url('~@/assets/portraits/Heidi.jpg?width=300&height=300');
 }
 .laura {
-  background-image: url('~@/assets/portraits/laura.jpg?width=300&height=300');
+  background-image: url('~@/assets/portraits/Laura.jpg?width=300&height=300');
 }
 .giorgio {
-  background-image: url('~@/assets/portraits/giorgio.jpg?width=300&height=300');
+  background-image: url('~@/assets/portraits/Giorgio.jpg?width=300&height=300');
 }
 .richard {
-  background-image: url('~@/assets/portraits/richard.jpg?width=300&height=300');
+  background-image: url('~@/assets/portraits/Richard.jpg?width=300&height=300');
 }
 </style>
 
