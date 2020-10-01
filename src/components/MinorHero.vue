1462b738b805733280c59d2c883b5740630093db
Joe Schr <joe.vandelay@gmail.com>
2020-10-01 21:33:07 +0200

    fix(hydration fail): invalid html because of root link in minorHero

--- src/components/MinorHero.vue
+++ src/components/MinorHero.vue
@@ -1,9 +1,10 @@
 <template lang="pug">
   section
-    g-link(:to='link' ).flex.flex-wrap.items-center.max-w-5xl.mx-auto.my-6
+    .flex.flex-wrap.items-center.max-w-5xl.mx-auto.my-6
       .px-8.w-full(:class="{'order-1':!isLeft}" class='md:w-1/2 md:h-3/5')
         .hidden.max-w-sm.mx-auto(class='md:block', fit='inside', width='250', height='250')
-          slot
+          g-link(:to='link' )
+            slot
       .px-8(class='md:w-1/2 md:mb-0').align-top
         h3.text-2xl.mb-2.mt-8.font-heading(class="lg:text-4xl lg:mt-0") {{ title }}
         p.mb-4.text-gray-600.leading-relaxed {{ content }}
