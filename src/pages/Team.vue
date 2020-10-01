57e9820159d1ea5fedd30bc1e77ba09317dd09f2
Joe Schr <joe.vandelay@gmail.com>
2020-10-01 16:55:40 +0200

    team with pictures from yaml responsive

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -1,99 +1,77 @@
-<template>
-  <Layout>
-    <section class="py-12 px-4">
-      <div class="flex flex-wrap -mx-4 mb-8">
-        <div class="lg:w-1/2 w-full px-4 mb-4 lg:mb-0">
-          <g-image
-            class="rounded shadow-md mx-auto"
-            src="~/assets/placeholders/pictures/male_avatar.svg"
-            alt=""
-            width="200"
-            fit="inside"
-            height="200"
-          />
-        </div>
-        <div class="lg:w-1/2 w-full px-4">
-          <h2 class="text-4xl font-heading font-semibold mx-auto">
-            Michael Scott
-          </h2>
-          <span class="text-xl">Regional Manager</span>
-          <p class="my-5">
-            Former branch manager of the Scranton branch of Dunder Mifflin Paper
-            Company, The Michael Scott Paper Company, and later Dunder Mifflin
-            Sabre. Michael enjoys writing song parodies.
-          </p>
-          <p class="my-5">
-            Michael was born on March 15, 1962, at 11:23 AM, in Scranton,
-            Pennsylvania. He was raised by his mother and stepfather, Jefferson
-            (“Jeff”), with a home on Kenneth Road.
-          </p>
-          <div class="flex">
-            <img
-              class="w-8 mr-6"
-              src="placeholders/icons/message.svg"
-              alt=""
-            /><img
-              class="w-8 mr-6"
-              src="placeholders/icons/share.svg"
-              alt=""
-            /><img class="w-8 mr-6" src="placeholders/icons/star.svg" alt="" />
-          </div>
-        </div>
-      </div>
-      <div class="flex flex-wrap -mx-4">
-        <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
-          <img
-            class="rounded shadow-md"
-            src="placeholders/pictures/female_avatar.svg"
-            alt=""
-          />
-        </div>
-        <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
-          <img
-            class="rounded shadow-md"
-            src="placeholders/pictures/male_avatar.svg"
-            alt=""
-          />
-        </div>
-        <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
-          <img
-            class="rounded shadow-md"
-            src="placeholders/pictures/female_avatar.svg"
-            alt=""
-          />
-        </div>
-        <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
-          <img
-            class="rounded shadow-md"
-            src="placeholders/pictures/male_avatar.svg"
-            alt=""
-          />
-        </div>
-        <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
-          <img
-            class="rounded shadow-md"
-            src="placeholders/pictures/female_avatar.svg"
-            alt=""
-          />
-        </div>
-        <div class="w-1/2 lg:w-1/6 px-4 mb-4 lg:mb-0">
-          <img
-            class="rounded shadow-md"
-            src="placeholders/pictures/male_avatar.svg"
-            alt=""
-          />
-        </div>
-      </div>
-    </section>
-  </Layout>
+<template lang="pug">
+Layout
+  .divide-y.divide-gray-400
+    section.py-12.px-4.container.py-2(v-for='(person,index) in team' :key='person.id').pt-8
+      .flex.flex-wrap.-mx-4.mb-8
+        .portrait.px-4.mb-4(class="lg:w-1/2 lg:mb-0 h-32" :class="person.id")
+        .w-full.px-4(class='lg:w-1/2')
+          h2.text-4xl.font-heading.font-semibold.text-center.mx-auto.
+            {{ person.name }}
+          .py-8.px-4
+            .max-w-2xl.pl-12.py-4.mx-auto.border-l-4
+              p.text-xl.mb-6 {{ person.zitat }}
+              p.text-sm
+                span.mr-2  {{ person.zitatAuthor}}
+                //- span.font-semibold CEO at Realweb
+      .flex.flex-wrap.-mx-4.w-full
+        .px-4.mb-4(class='lg:mb-0 lg:w-1/3 w-1/2')
+          h3.text-xl Sprachraum
+          p.tx-sm(v-html="toBr(person.sprachraum)")
+        .px-4.mb-4(class='lg:mb-0 lg:w-1/3 w-1/2')
+          h3.text-xl Kurzbio
+          p.tx-sm(v-html="toBr(person.kurzbio)")
+        .px-4.mb-4(class='lg:mb-0 lg:w-1/3 w-1/2')
+          h3.text-xl Schlagworte
+          p.tx-sm(v-html="schlagworte(person)")
 </template>
 
 <script>
+import team from '@/content/Team.yaml'
+
 export default {
   metaInfo: {
     title: 'Team',
   },
+  data() {
+    return {
+      team,
+    }
+  },
+  methods: {
+    toBr: (str) => (str ? str.replace(/(?:\r\n|\r|\n)/g, '<br>') : ''),
+    schlagworte(person) {
+      return this.toBr(
+        `Verb: ${person.verb}\nSubstantiv: ${person.substantiv}\nAdjektiv: ${person.adjektiv}`
+      )
+    },
+  },
 }
 </script>
 
-<style></style>
+<style scoped>
+.portrait {
+  height: 300px;
+  width: 300px;
+  background-size: contain; /* <------ */
+  background-repeat: no-repeat;
+  background-position: center center; /* optional, center the image */
+}
+.elke {
+  background-image: url('~@/assets/portraits/elke.jpg?width=300&height=300');
+}
+.johannes {
+  background-image: url('~@/assets/portraits/johannes.jpg?width=300&height=300');
+}
+.heidi {
+  background-image: url('~@/assets/portraits/heidi.jpg?width=300&height=300');
+}
+.laura {
+  background-image: url('~@/assets/portraits/laura.jpg?width=300&height=300');
+}
+.giorgio {
+  background-image: url('~@/assets/portraits/giorgio.jpg?width=300&height=300');
+}
+.richard {
+  background-image: url('~@/assets/portraits/richard.jpg?width=300&height=300');
+}
+</style>
