3280922e17172de483a5cd947a7ca46c62ce20ff
Joe Schr <joe.vandelay@gmail.com>
2020-10-01 20:58:07 +0200

    fix(MinorHero): isLeft was reversed

--- src/components/MinorHero.vue
+++ src/components/MinorHero.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   section
     g-link(:to='link' ).flex.flex-wrap.items-center.max-w-5xl.mx-auto.my-6
-      .px-8.w-full(:class="{'order-1':isLeft}" class='md:w-1/2 md:h-3/5')
+      .px-8.w-full(:class="{'order-1':!isLeft}" class='md:w-1/2 md:h-3/5')
         .hidden.max-w-sm.mx-auto(class='md:block', fit='inside', width='250', height='250')
           slot
       .px-8(class='md:w-1/2 md:mb-0').align-top
