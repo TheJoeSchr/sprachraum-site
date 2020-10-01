9857c870750cb2af5e968a770d16ef3f3a9a663b
Joe Schr <joe.vandelay@gmail.com>
2020-10-01 11:16:51 +0200

    swap days and price, make button smaller

--- src/components/KursprogrammKarte.vue
+++ src/components/KursprogrammKarte.vue
@@ -3,16 +3,16 @@ section(class="py-0")
   .flex.flex-wrap.max-w-4xl.mx-auto
     .w-full.py-1.mb-1.px-2(class='md:w-full md:mb-0')
       .py-1.border
-        div.ml-auto.text-center(class='md:mx-32 w-1/5')
-          p.mb-0.leading-relaxed.text-sm.py-1.px-3.mt-4.mr-2.text-black.font-secondary.bg-secondary-500.rounded-full.
-            {{ value.cost }}
+        div.ml-auto.text-center(class='md:mx-32 w-2/5')
+          p.mb-2.leading-relaxed.text-sm.py-1.px-3.mt-1.mr-2.text-black.font-secondary.bg-secondary-200.rounded-full.
+            {{ value.days }}
 
         h3.text-lg.pl-3.mb-3.font-heading.font-semibold.left-0(class="w-full") {{value.name}}
           h4.mb-3.text-sm.font-heading.font-semibold {{ value.date }}
-          div.pr-3.text-sm.text-gray-500.w-full.text-right
-            .ml-auto.mb-1(class="w-2/5") {{ value.days }}
-        .ml-auto.mb-2.px-2
-          a(:href='mailto(value)' class='hover:bg-indigo-200').inline-block.w-full.py-3.px-5.leading-none.text-center.text-primary-900.bg-primary-100.rounded.shadow Buchen
+          div.pr-3.text-md.text-gray-900.w-full.text-right
+            .ml-auto.mb-1(class="w-2/5") {{ value.cost }}
+        .mx-auto.mb-2(class='w-2/5')
+          a(:href='mailto(value)' class='hover:bg-indigo-200').inline-block.w-full.py-3.px-5.leading-none.text-center.text-primary-900.bg-primary-100.rounded.shadow BUCHEN
 </template>
 
 <script>
