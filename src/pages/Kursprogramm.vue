bf86bcbf213e584b6bf86af1238226345c8e20b0
Joe Schr <joe.vandelay@gmail.com>
2020-10-01 11:43:40 +0200

    add kurs data

--- src/pages/Kursprogramm.vue
+++ src/pages/Kursprogramm.vue
@@ -3,8 +3,8 @@ Layout
   .divide-y.divide-gray-400
     .container.py-2(v-for='(section,index) in timetable' :key='section.title').pt-8
       section(:class="{'bg-yellow-100':(index%2==1), 'shadow-lg':(index%2==1) ,'shadow-inner':(index%2==0) }" class="mb-0").my-2.pt-1
-        h2.text-center.text-xl.py-2.font-heading.font-semibold {{ section.title }}
-        h3.text-center.text-lg.py-2.font-heading.font-semibold {{ section.subtitle }} von {{ section.times}}
+        h2.text-center.text-xl.pt-2.font-heading.font-bold {{ section.title }}
+        h3.text-center.text-lg.pb-1.font-heading {{ section.subtitle }} von {{ section.times}}
         ul
           li(v-for='course in section.courses' :key="course.name")
             KursprogrammKarte(:value="course")
