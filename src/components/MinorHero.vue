65c1cf1aa01955f50efd7d89973a56ee09353f98
Joe Schr <joe.vandelay@gmail.com>
2020-10-01 19:23:57 +0200

    add hero and re-add kursprogramm

--- src/components/MinorHero.vue
+++ src/components/MinorHero.vue
@@ -1,17 +1,14 @@
 <template lang="pug">
   section
     g-link(:to='link' ).flex.flex-wrap.items-center.max-w-5xl.mx-auto.mb-8
-      .pt-8.px-8.w-full(v-if="isLeft" class='md:w-1/2 md:h-3/5')
+      .pt-3.px-8.w-full(:class="{'order-1':isLeft}" class='md:w-1/2 md:h-3/5')
         .hidden.max-w-sm.mx-auto(class='md:block', fit='inside', width='250', height='250')
           slot
-      .px-8.mb-8(class='md:w-1/2 md:mb-0')
-        h3.text-2xl.mb-2.font-heading {{ title }}
+      .pt-3.px-8(class='md:w-1/2 md:mb-0')
+        h3.text-2xl.mb-2.mt-8.font-heading {{ title }}
         p.mb-4.text-gray-600.leading-relaxed.
           {{ content }}
         g-link.text-primary-700(:to='link', class='hover:underline') {{ linkText }}
-      .pt-8.px-8.w-full(v-if="!isLeft" class='md:w-1/2 md:h-3/5')
-        .hidden.max-w-sm.mx-auto(class='md:block', fit='inside', width='250', height='250')
-          slot
 </template>
 
 <script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,14 +3,15 @@ Layout
   section
     .flex.flex-wrap.items-center.text-center.mx-2(class='md:text-left md:my-8')
       .px-2.order-1(class='md:w-1/2 md:pr-10 md:order-none')
-        h2.text-2xl.pb-2.leading-tight.font-heading(class='md:text-5xl md:pb-0') Unsere Deutschkurse könnt ihr ab sofort Online besuchen
-        p.mb-8.text-gray-600.leading-relaxed Trefft Elke, Laura &amp; Kolleg_innen live in den virtuellen Work IT AUT Klassenzimmern.
-        .mb-2
+        h2.text-3xl.py-4.mt-4.leading-tight.font-heading(class='md:text-5xl md:pb-0').
+          {{ hero.title }}
+        p.mb-8.text-gray-600.leading-relaxed.
+          {{ hero.content }}
+        .mb-8
           g-link.inline-block.py-4.px-8.mr-6.leading-none.text-black.bg-primary-500.rounded.shadow(
-            to="/kursprogramm/"
+            to='/kursprogramm/'
             class='hover:bg-primary-600'
-          ) Gleich anmelden!
-            //- a(href="#" class="text-secondary-700 hover:underline") >> Einstufungstest
+          ) {{ hero.linkText }}
 
 
       .px-2.hidden(class='md:w-1/2 md:block')
@@ -36,18 +37,25 @@ export default {
     title: 'Home',
   },
   data: () => ({
+    hero: {
+      title: 'Unsere Deutschkurse könnt ihr ab sofort Online besuchen!',
+      content:
+        'Trefft Elke, Laura, Johannes, Heidi & ihre Kolleg*innen in den virtuellen Klassenzimmern von Work IT AUT',
+      link: 'kursprogramm',
+      linkText: 'Gleich anmelden!',
+    },
     cards: [
-      {
-        title: 'Kursprogramm',
-        link: '/kursprogramm/',
-        linkText: '>> Mehr Infos',
-        content: `A1–C1`,
-      },
       {
         title: 'Deutschkurse',
         link: '/deutschkurse/',
         linkText: '>> Mehr Infos',
-        content: `A1–C1`,
+        content: `Wir legen im Unterricht großen Wert darauf, dass alle vier wichtigen Kernbereiche trainiert werden: die rezeptiven LESEN und HÖREN und die produktiven SPRECHEN und SCHREIBEN. `,
+      },
+      {
+        title: 'Kursprogramm',
+        link: '/kursprogramm/',
+        linkText: '>> Studenplan & Preise',
+        content: `Die Lebens- und Arbeitswelten sind in den letzten Monaten durcheinander geraten. Wir haben unser Projekt erweitert und freuen uns euch ab sofort unsere DAF/DAZ Kurse ONLINE &amp; LIVE anbieten zu können`,
       },
     ],
   }),
