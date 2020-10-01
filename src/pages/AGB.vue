47bc394d502afd19bebb0910e295128629a9607b
Joe Schr <joe.vandelay@gmail.com>
2020-10-01 13:25:21 +0200

    add agb page

---
+++ src/pages/AGB.vue
@@ -0,0 +1,162 @@
+<template lang="pug">
+layout
+  section.py-12.px-4
+    .max-w-4xl.mx-auto
+      h2.text-4xl.text-center.mb-12.font-heading Allgemeine Geschäftsbedingungen
+      div.text-lg.pb-4.
+        Nachstehend die für das Kalenderjahr 2020/2021 gültigen Allgemeinen
+        Geschäftsbedingungen für Sprachraum – Work IT AUT ein in Projekt der KompeTrend
+        GmbH, Linz OÖ / Österreich. Im folgenden kurz WIA KompeTrend GmbH
+
+      .flex.flex-wrap.-mx-8
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 1. Geltungsbereich
+          p.
+            Die Geschäftsbedingungen gelten für alle Angebote, wie Standardkurse, Deutsch als
+            Berufsprache und Coachings, die von WIA KompeTrend GmbH angeboten werden.
+            Ausgenommen sind Firmenkurse, die von Unternehmen in Auftrag gegeben werden.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 2. Anmeldung / Vertragsabschluss
+          p.
+            Die Anmeldungen sind per Email, durch das Klicken (und senden) der Anmeldung auf
+            unseren Button auf der Hompage oder telefonisch möglich. Alle Anmeldungen werden von
+            WIA KompeTrend GmbH bestätigt.
+            Die Anmeldebestätigung erfolgt via Email und mit dieser Bestätigung entsteht ein Vertrag.
+            Erst wenn der Kursbeitrag (oder die erste Ratenzahlung) bei uns eingegangen ist, können wir
+            Ihnen einen Kursplatz garantieren. (Siehe Bestimmungen durch Ratenzahlung)
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 3. Teilnahmevoraussetzungen
+          p.
+            3.1 Die Teilnahmeberechtigung impliziert das Vorliegen allfällig festgelegter Qualifikationen
+            und Altersstufen sowie der gesetzlich normierten Bedingungen voraus.
+            Die Beurteilung des Vorliegens dieser Voraussetzungen liegt einzig im Ermessen von WIA
+            KompeTrend GmbH
+          p.
+            3.2 Um den entsprechenden Lernerfolg zu erzielen braucht es folgende Voraussetzungen:
+            Eine stabile Internetverbindung (Fiber etc) und einen geübten Umgang mit dem Computer
+            bzw. mit Internetplattformen.
+          p.
+            3.3 Für sämtliche Teilnehmer_innen der Deutsch A1 Kurse sind gute Englisch oder
+            Italienischkenntnisse notwendig, um den Einstieg in das Kursgeschehen von Anfang an zu
+            verstehen. Dies gilt nicht für sprachhomogene Gruppen.
+          p.
+            3.3 Für sämtliche Teilnehmer_innen der Deutsch A1 Kurse sind gute Englisch oder
+          p.
+            3.4 Es dürfen – ohne ausdrückliche Erlaubnis der Trainer_innen – keine Ton- u.
+            Videoaufnahmen gemacht werden.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 4. Kursangebote &amp; Coachings
+          p.
+            4.1 Die Zeiteinheit unserer Veranstaltungen sind als „EH“ bei allen Angeboten auf der
+            Website angegeben und umfassen jeweils 50 Minuten.
+          p.
+            4.2 Gruppenkurse sind auf ein Minimum von 3 und auf ein Maximum von 6
+            Teilnehmer_innen beschränkt.
+          p.
+            4.3 Etwaiige Änderungen sind extra auf dem jeweiligen Angebot gekennzeichnet. Auch die
+            Dauer der Kurse ist im Angebot klar definiert und gilt als verbindlich.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 5. Rechtliches für Foto-, Film- und Tonaufnahmen
+          p.
+            5.1 Wir verwenden Foto-, Film- und Tonaufnahmen von unseren Kursen und Veranstaltungen
+            für Werbe- und Informationsmaterialien auf unserer Website, in unseren Social Media
+            Kanälen und für Printproduktionen. Sie stimmen zu.
+          p.
+            5.2 Wenn Sie das nicht möchten, geben Sie dies bitte zu Kursbeginn bzw. vor
+            Veranstaltungsbeginn bekannt.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 6. Kursänderungen: Termine und Absagen
+          p.
+            6.1 Sollte ein Kurs aufgrund zu geringer TN_innen Zahl nicht zustande kommen, prüfen wir
+            die Möglichkeit bzw. haben wir das Recht den Tarif entsprechend der zu geringen TN_innen
+            Zahl zu erhöhen oder die Kurszeit zu verkürzen. Dies geschieht immer in Absprache mit den
+            angemeldeten TN_innen vor Kursbeginn.
+          p.
+            6.2 Sollte es für einen Kurs oder eine Veranstaltung von WIA KompeTrend GmbH zu nicht
+            ausreichend vielen Anmeldungen kommen, haben wir das Recht den Kurs abzusagen. Die
+            angemeldeten TN_innen bekommen in diesem Fall 100% der Kursgebühr zurückerstattet.
+          p.
+            6.3 Fällt ein Kurs durch Krankheit oder Abwesenheit einer Trainer_in aus, sorgt WIA
+            KompeTrend GmbH für eine_n geeignete_n Ersatztrainer_in. So das nicht möglich ist, wird
+            die ausgefallene Einheit entweder an einem anderen Termin (akkordiert mit der gesamten
+            Gruppe) oder nach Kursende nachgeholt.
+          p.
+            6.4 TN_innen, die aufgrund von persönlichen Gründen, durch Krankheit an einem Termin
+            nicht teilnehmen können, kann WIA KompeTrend GmbH keinen Ersatztermin anbieten.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 7. Teilnahmebestätigungen
+          p.
+            7.1 Am Ende der Gruppenkurse erhalten Sie eine Teilnahmebestätigung über Umfang und
+            Inhalt des von Ihnen besuchten Kurses. Bedingung: 70% Anwesenheit.
+          p.
+            7.2 Für Coachings erhalten Sie auf Wunsch eine Teilnahmebestätigung.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 8. Zahlungsbedingungen
+          p.
+            8.1 Die Kursgebühr ist vor Kursbeginn an das auf der Anmeldebestätigung angegebene Konto
+            von WIA KompeTrend GmbH einzuzahlen. Bei einer Ratenvereinbarung ist die 1. Rate vor
+            Kursbeginn fällig.
+          p.
+            8.2 Kursbücher sind im Kurspreis nicht inbegriffen. Sie müssen selbst organisiert, bestellt und
+            gekauft werden. Wir schicken rechtzeitig vor Kursbeginn die ISBN an sie.
+          p.
+            8.3 Die Kurse können in max. 2 Raten bezahlt werden. Wenn sie eine zweimalige
+            Ratenzahlung möchten, geben Sie dies bei der Anmeldung oder via Email bekannt. Sie
+            bekommen mit der Anmeldebestätigung einen Ratenplan, und den Termin bis wann die 2.
+            Rate fällig ist. Die 1. Rate beträgt 60% der gesamten Kursgebühr.
+          p.
+            8.3.1 Ausgenommen von der Ratengebühr sind Coachings und Minikurse. Über die
+            Gewährung einer Ratenzahlung entscheidet ausschließlich WIA Kompetrend GmbH
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 9 Ermäßigungen
+          p.
+            Unsere Stamm-Schüler_innen bekommen ihre Treue belohnt
+            ab dem 2. Kurs, eines Coachings oder sonstiger Angebote: minus 5%
+            ab dem 4. Kurs, eines Coachings oder sonstiger Angebote: minus 10%
+          p.
+            9.1 Weiter: wenn ein Familienmitglied im selben Kalenderjahr
+            einen Kurs besucht: minus 7%
+          p.
+            9.2 Wenn Sie den Kurs aufgrund eines Arbeitseinsatzes bei einem Partner oder best friend -
+            Unternehmen von WIA KompeTrend GmbH in Österreich absolvieren
+            minus 15%
+          p.
+            9.3 Wenn Sie Mitarbeiter_in eines Partners oder best friend Unternehmens von WIA
+            KompeTrend GmbH in Österreich oder Italien sind: minus 10%
+          p.
+            9.4 Schüler_innen &amp; Student_innen minus 5 %
+            Alle Ermäßigungen gelten ausschließlich für Gruppenkurse. Ermäßigungen für Coachings
+            und etwaiige andere Individual-Termin Veranstaltungen können nicht gewährt werden.
+            Die Ermäßigungen gelten nicht, wenn die Mindestteilnehmer_innen Anzahl nicht erreicht
+            wird.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 10 RÜCKTRITTSRECHT / STORNO
+          p.
+            10.1 Sie können bis zu 5 Werktage vor Kursbeginn kostenfrei von ihrem Vertrag zurücktreten
+          p.
+            10.2 Bei Rücktritt zwischen 4. Tag und dem 1. Tag des Kurses (Kursbeginn) verrechnen wir
+            40% der Kurskosten
+          p.
+            10.3 Ab dem 2. Kurstag ist keine Rückzahlung mehr möglich.
+          p.
+            10.4 Sollten Sie ohne sich zu melden dem Kurs fernbleiben fällt die gesamte Kursgebühr an.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 12 Ich akzeptiere die Allgemeinen Geschäftsbedingungen
+          p.
+            in der hier vorliegenden Fassung und stimme den darin enthaltenen Bestimmungen zu.
+
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 13 Mit der Akzeptanz der AGB von WIA KompeTrend GmbH
+          p.
+            stimmt der/die Vertragspartner_in ausdrücklich dem Erhalt von elektronischem Informations- und
+            Werbematerial an der von ihr/ihm bekannt gegebenen E-Mail-Adresse zu. Diese kann
+            jederzeit schriftlich widerrufen werden.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 14 Für sämtliche Rechtsstreitigkeiten
+          p wird die Zuständigkeit des sachlich zuständigen Gerichts von KompeTrend GmbH vereinbart.
+        .px-8.mb-8(class="lg:w-1/2")
+          h3.text-lg.mb-1.font-heading.font-semibold 15 Auf sämtliche Rechtsstreitigkeiten aus dieser Vereinbarung
+          p ist ausschließlich österreichisches Recht anwendbar.
+</template>
+
+<style scoped></style>
