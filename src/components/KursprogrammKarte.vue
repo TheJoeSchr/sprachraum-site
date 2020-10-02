82731644774cbfb5c539e8af1a42950ae9a15c07
Joe Schr <joe.vandelay@gmail.com>
2020-10-02 10:40:19 +0200

    courses: add double start to cost

--- src/components/KursprogrammKarte.vue
+++ src/components/KursprogrammKarte.vue
@@ -7,12 +7,12 @@ section(class="pt-2")
         h3.text-lg.pl-3.mb-1.font-heading.font-bold.left-0(class="w-full") {{name}}
         h4.pl-3.mb-3.text-sm.font-heading.font-semibold  {{ date }}
         div.pr-3.mr-3.text-lg.text-gray-900.text-right
-          .ml-auto.mb-1(class="w-2/5") {{ cost }}
+          .ml-auto.mb-1(class="w-2/5") {{ cost }}**
         div.ml-auto.text-right(class='w-2/5 ')
           p.leading-relaxed.text-sm.px-3.mr-2.text-gray-800.font-secondary
             | {{ days }}
           p.mb-2.leading-relaxed.text-sm.px-3.mr-2.text-gray-600.font-secondary
-            | {{times }}
+            | {{ times }}
         .mx-auto.mb-2(class='w-2/5')
           a(:href='mailto(value)' class='hover:bg-secondary-200').inline-block.w-full.py-3.px-5.leading-none.text-center.font-bold.text-primary-900.bg-primary-200.rounded.shadow.
             {{ messages.btnBooking }}
