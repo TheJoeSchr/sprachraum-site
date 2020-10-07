3a0e251d3a763ff47a57f44280a36ee024236d54
Joe Schr <joe.vandelay@gmail.com>
2020-10-07 08:59:08 +0000

    feature: add tba/underconstruction site

---
+++ src/pages/TBA.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+Layout
+  p
+    strong TBA
+    br
+    |        UNDER CONSTRUCTION
+</template>
+<style scoped>
+h3 {
+  @apply text-xl;
+}
+h4 {
+  @apply text-lg;
+}
+</style>
