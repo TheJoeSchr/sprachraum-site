f24223e446b14fd7e9a304d4ebfcb6f290d91471
Joe Schr <joe.vandelay@gmail.com>
2021-08-31 21:30:41 +0200

    feature: add carriere page

---
+++ src/pages/it/Carriere.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+Layout
+  Info(:value="{...content, translateLink:'/deutschkurse/'}")
+    g-image(src='~/assets/images/1_Deutschkurs_blicksalzburg-1133549562.jpg' height=300)
+</template>
+<script>
+import Info from '~/components/CarriereInfo.vue'
+import content from '~/content/it/Carriere.yaml'
+import { message } from '~/content/it/Interface.yaml'
+
+export default {
+  components: {
+    Info,
+  },
+  metaInfo: {
+    title: 'Carriere',
+  },
+  data() {
+    return {
+      content: {
+        ...content,
+        messages: message,
+      },
+    }
+  },
+}
+</script>
