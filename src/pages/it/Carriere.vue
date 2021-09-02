68a105357d95663e45fc121ba5643949452f0012
Joe Schr <joe.vandelay@gmail.com>
2021-09-02 18:47:30 +0200

    add Carriere copytext

--- src/pages/it/Carriere.vue
+++ src/pages/it/Carriere.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 Layout
   Info(:value="{...carriere, translateLink:'/deutschkurse/'}")
-    g-image(src='~/assets/images/sonnenterrasse-1125943452.jpg' height=200 width=200)
+    g-image(src='~/assets/images/sonnenterrasse-1125943452.jpg' height=200 width=300)
   Info(:value="{...beratung, translateLink:'/deutschkurse/'}")
     g-image(src='~/assets/images/sonnenterrasse-1125943452.jpg' height=200 width=200)
 </template>
 <script>
 import Info from '~/components/CarriereInfo.vue'
 import carriere from '~/content/it/Carriere.yaml'
-import beratung from '~/content/it/Carriere.yaml'
+import beratung from '~/content/it/Beratung.yaml'
 import { message } from '~/content/it/Interface.yaml'
 
 export default {
