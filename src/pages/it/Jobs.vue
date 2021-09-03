defbde802a98b51054f9d951cb97d6065aad6d11
Joe Schr <joe.vandelay@gmail.com>
2021-09-03 15:14:10 +0200

    add new pages Abos, Jobs, Services

---
+++ src/pages/it/Jobs.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+Layout
+  Info(:value="{...carriere, }")
+    g-image(src='~/assets/images/sonnenterrasse-1125943452.jpg' height=200 width=300)
+  Info(:value="{...beratung, }")
+    g-image(src='~/assets/images/schneemann-119510173.jpg' height=200 width=300)
+</template>
+<script>
+import Info from '~/components/CarriereInfo.vue'
+import carriere from '~/content/it/Carriere.yaml'
+import beratung from '~/content/it/Beratung.yaml'
+import { message } from '~/content/it/Interface.yaml'
+
+export default {
+  components: {
+    Info,
+  },
+  metaInfo: {
+    title: 'Carriere',
+  },
+  data() {
+    return {
+      carriere: {
+        ...carriere,
+        messages: message,
+      },
+      beratung: {
+        ...beratung,
+        messages: message,
+      },
+    }
+  },
+}
+</script>
---
+++ src/pages/it/Services.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+Layout
+  Courses(:value="{...content, translateLink:'/kursprogramm/'}")
+</template>
+
+<script>
+import Courses from '~/components/Courses.vue'
+import timetable from '~/content/it/Calendario.yaml'
+import { message } from '~/content/it/Interface.yaml'
+
+export default {
+  components: {
+    Courses,
+  },
+  metaInfo: {
+    title: 'Calendario',
+  },
+  data() {
+    return {
+      content:{
+        timetable,
+        messages: message,
+      }
+    }
+  }
+}
+</script>
