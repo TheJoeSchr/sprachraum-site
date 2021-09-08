9f01bfdaf9be7c27749f9718d3b2e13139f68eb8
Joe Schr <joe.vandelay@gmail.com>
2021-09-08 18:54:35 +0200

    feature: merge jobs & carriere

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -16,7 +16,6 @@ div
         div(class="navbar-menu hidden lg:flex lg:flex-grow lg:items-center w-full lg:w-auto")
             div(class="lg:ml-auto")
               g-link(to="/"  class="block lg:inline-block mt-4 lg:mt-0 mr-10 text-gray-900 hover:text-primary-700") home
-              g-link(to="/it/carriere/"  class="block lg:inline-block mt-4 lg:mt-0 mr-10 text-gray-900 hover:text-primary-700") carriere
               g-link(to="/team/"  class="block lg:inline-block mt-4 lg:mt-0 mr-10 text-gray-900 hover:text-primary-700") über uns
               g-link(to="/it/services/"  class="block lg:inline-block mt-4 lg:mt-0 mr-10 text-gray-900 hover:text-primary-700") services
               g-link(to="/it/jobs/"  class="inline-block py-3 px-5 mr-10 leading-none text-center text-gray-700 bg-secondary-100 hover:bg-secondary-300 rounded shadow") jobs in AUT
--- src/pages/it/Jobs.vue
+++ src/pages/it/Jobs.vue
@@ -2,9 +2,12 @@
 Layout
   InfoPage(:value="{...jobs, }")
     g-image(src='~/assets/images/sonnenterrasse-1125943452.jpg' height=200 width=300)
+  InfoPage(:value="{...carriere, }")
+    g-image(src='~/assets/images/schneemann-119510173.jpg' height=200 width=300)
 </template>
 <script>
 import InfoPage from '~/components/InfoPage.vue'
+import carriere from '~/content/it/Carriere.yaml'
 import jobs from '~/content/it/Jobs.yaml'
 import { message } from '~/content/it/Interface.yaml'
 
@@ -17,6 +20,10 @@ export default {
   },
   data() {
     return {
+      carriere: {
+        ...carriere,
+        messages: message,
+      },
       jobs: {
         ...jobs,
         messages: message,
