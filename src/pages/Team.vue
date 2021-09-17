fa28d58eb06b522f3d5bd14e23ab7a06d5134468
Joe Schr <joe.vandelay@gmail.com>
2021-09-17 13:54:11 +0200

    Team: change verben -> parole chiave

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -23,7 +23,7 @@ Layout
           h3.text-xl Sprachraum WORK IT AUT
           p.tx-sm.text-gray-700(v-html="toBr(person.sprachraum)")
         .column-section(class='md:-mx-4')
-          h3.text-xl Verben
+          h3.text-xl parole chiave 
           p.tx-sm.text-gray-700(v-html="schlagworte(person)")
 </template>
 <script>
