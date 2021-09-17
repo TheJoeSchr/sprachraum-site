705710da012b032196f5628f15cf39531dd1858d
Joe Schr <joe.vandelay@gmail.com>
2021-09-17 17:25:06 +0200

    feature: add DreamJobInfo wip

--- src/pages/it/Jobs.vue
+++ src/pages/it/Jobs.vue
@@ -2,12 +2,17 @@
 Layout
   #jobs
     section.dreamjobs
-          <!-- .max-w-7xl.mx-auto.px-8.pt-4(class='sm:px-6 lg:px-8') -->
-          <!--   h3.text-2xl.mb-2.mt-8.font-heading(class="lg:text-4xl lg:mt-0") Jobs -->
-          .mx-6(class='sm:py-4 lg:py-2 lg:max-w-none')
-            .mt-2(class='lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6')
-              .group(v-for='(dreamjob, index) in jobs.dreamjobs')
-                DreamJobCard(:value='dreamjob')
+      <!-- .max-w-7xl.mx-auto.px-8.pt-4(class='sm:px-6 lg:px-8') -->
+      <!--   h3.text-2xl.mb-2.mt-8.font-heading(class="lg:text-4xl lg:mt-0") Jobs -->
+      .mx-6(class='sm:py-4 lg:py-2 lg:max-w-none')
+        .mt-2(class='lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6')
+          template(v-for='(dreamjob, index) in jobs.dreamjobs')
+            .group(v-on:click="toggleInfo(dreamjob)")
+              DreamJobCard(:value='dreamjob')
+
+
+        section.dreamjobinfo.bg-white(class='sm:py-4 lg:py-2 lg:max-w-none' v-show="currentInfo" @click="toggleInfo()")
+          DreamJobInfo(v-if="currentInfo" :value='currentInfo')
     InfoPage(:value='jobs')
       g-image(src='~/assets/images/sonnenterrasse-1125943452.jpg' height=200 width=300)
     InfoPage(:value='{...carriere, }')
@@ -15,6 +20,7 @@ Layout
 </template>
 <script>
 import DreamJobCard from '~/components/DreamJobCard.vue'
+import DreamJobInfo from '~/components/DreamJobInfo.vue'
 import InfoPage from '~/components/InfoPage.vue'
 import MinorHero from '~/components/MinorHero.vue'
 import carriere from '~/content/it/Carriere.yaml'
@@ -25,14 +31,12 @@ import { toBr } from '@/helpers'
 export default {
   components: {
     DreamJobCard,
+    DreamJobInfo,
     InfoPage,
     MinorHero,
   },
   metaInfo: {
     title: 'Jobs',
-  },
-  methods: {
-    toBr
   },
   data() {
     return {
@@ -44,7 +48,16 @@ export default {
         ...jobs,
         messages: message,
       },
+      showInfo: false,
+      currentInfo: null
     }
+  },
+  methods: {
+    toBr,
+    toggleInfo(dreamjob){
+      this.currentInfo = this.currentInfo? null : dreamjob
+    }
+
   },
 }
 </script>
