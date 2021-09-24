b128cfe55639b064fa24351ce869c8d4b5df021d
Joe Schr <joe.vandelay@gmail.com>
2021-09-24 19:29:28 +0200

    Job: envelop pics

--- src/components/DreamJobInfo.vue
+++ src/components/DreamJobInfo.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
 section.dreamjobinfo(v-if='dreamjob')
   .flex.flex-wrap.mx-2.mb-6
-    g-image.img(:src="require(`!!assets-loader!@jobs/${dreamjob.id}.jpg`)" class='md:mx-0 md:w-1/3 md:mb-0 md:pr-8' height=300 width="300" center="center" fit="cover")
-    .w-full.px-4.mx-4(class='md:w-1/2 ')
+    div.img.mx-auto(class='w-1/3')
+      g-image.img(:src="require(`!!assets-loader!@jobs/${dreamjob.id}.jpg`)" class='md:mx-0 md:w-1/3 md:mb-0 md:pr-8' height=300 width="300" center="center" fit="inside")
+    .w-full.px-4.mx-4(class='md:w-2/3 ')
       h2.text-xl.font-heading.font-semibold.text-center.mx-auto(class='md:text-4xl md:pt-0 md:text-left').
         {{ dreamjob.title }}
       h3.text-lg.font-heading.font-semibold.text-center.mx-auto.text-gray-600(class='md:text-4xl md:pt-0 md:text-left').
@@ -12,10 +13,10 @@ section.dreamjobinfo(v-if='dreamjob')
           .max-w-2xl.py-4.mx-auto
             p(v-html='toBr(dreamjob.description)')
   .more-info(class='md:mx-4')
-    a.more.my-4(@click='toggleInfo')
+    a.more.w-full.my-4(@click='toggleInfo')
       | {{ `More Infos  ${showInfo?'&times;':'&#8597;'}` }}
     div(class='flex flex-col')
-      .column-section.py-4(v-if='showInfo' v-for='column in dreamjob.columns' :key='column.title' class='')
+      .column-section.py-4(v-if='showInfo' v-for='column in dreamjob.columns' :key='column.title' class='flex-none md:flex-1')
           h3.text-normal {{ column.title }}
           p.text-sm.text-gray-700
             ul
