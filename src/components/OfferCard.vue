684abf6009aebfca015cdb8b9898706c98146343
Joe Schr <joe.vandelay@gmail.com>
2021-10-25 18:59:40 +0200

    OfferCard: add slices teaser text

--- src/components/OfferCard.vue
+++ src/components/OfferCard.vue
@@ -7,14 +7,16 @@ section(class="pt-2")
         Modal
           template(#inline)
               h3.text-xl.pl-3.mb-1.font-heading.font-bold.font-sans.left-0.uppercase(class="w-full" v-html='toBr(name)') 
-              div.pr-3.mr-3.text-lg.text-gray-900.text-center.underline(v-html="toBr(description.slice(0,80))+'<br>continuare...'")
+              div.px-3.mx-3.text-lg.text-gray-900.text-center(v-html="toBr(description && description.slice(0,80))")
+              br
+              .pr-3.mr-3.text-lg.text-gray-900.text-center.underline continuare...
               div.pr-3.mr-3.text-lg.text-gray-900.text-right
                 .ml-auto.mb-1(class="w-2/5" v-html="toBr(cost)")
               div.ml-auto.text-right(class='w-2/5 ')
                 p.leading-relaxed.text-sm.px-3.mr-2.text-gray-800.font-secondary(v-html='toBr(days)')
                 p.mb-2.leading-relaxed.text-sm.px-3.mr-2.text-gray-600.font-secondary(v-html='toBr(times)')
 
-          template(v-slot:default)
+          template(#default)
             h4.pl-3.mb-3.text-sm.font-heading.text-gray-800.font-medium.font-mono(v-html="toBr(description)")
         .mx-auto.mb-2(class='w-2/5' v-if="hideBooking != true")
           a(:href='mailtoFn({ ...value, legalText: value.messages.legalText })' class='hover:bg-secondary-200').inline-block.w-full.py-3.px-5.leading-none.text-center.font-bold.text-primary-900.bg-primary-200.rounded.shadow
