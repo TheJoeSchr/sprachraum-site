49b25f28f5f58427439ecc5c2ebdd48689395e69
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-04-14 17:14:28 +0200

    add JUNIORgoPRO to menu and placeholder page/content

---
+++ src/pages/it/Junior.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+Layout
+  div
+    template(v-for='(section,index) in cards' )
+      MinorHero(:value="{...section, isLeft: (index%2==0)}" )
+        g-image(v-if="0==index", src='~/assets/images/3_Kursprogramm_Sprachraum_1_iStock-1214385234.jpg' height=300 width="300"  center="bottom" fit="cover")
+        g-image(v-if="1==index", src='~/assets/images/WORK IT AUT-WORKBEE-LOGO-FINAL.jpg' height=300 width="300" center="top" fit="outside")
+        g-image(v-if="2==index", src='~/assets/images/schneemann-119510173.jpg' height=300 width="300" position="left" fit="cover")
+        g-image(v-if="3==index", src='~/assets/images/kocherei-stock-resort-kulinarik-genusshotel-05.jpg' height=300 width="300" center="top" fit="cover")
+        g-image(v-if="4==index", src='~/assets/images/2_Italienischkurs.jpg' height=300 width="300" center="top" fit="cover")
+
+    MinorHero(:value="{...contact, isLeft: true }" v-if="contact")
+      g-image.mx-auto.my-4( src='~/assets/images/contact.jpg' height="300" width="300" center="top" fit="cover")
+</template>
+
+<script>
+import MinorHero from '~/components/MinorHero.vue'
+import Layout from '~/layouts/Default.vue'
+import { toBr } from '~/helpers'
+import content from '~/content/it/Junior.yaml'
+export default {
+  name: 'JUNIORgoPRO',
+  components: {
+    Layout,
+    MinorHero,
+  },
+  data() {
+    return { ...content }
+  },
+  methods: {
+    toBr,
+  },
+}
+</script>
+
+<style scoped>
+section {
+  @apply mb-8 shadow bg-white border rounded overflow-hidden;
+}
+
+@screen md {
+  section {
+    @apply mb-16;
+  }
+}
+@screen lg {
+  section {
+    @apply py-0 px-8;
+  }
+}
+</style>
