8ac74486f3f953c721be49f5bccdc55e3b47c32e
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-05-04 15:32:44 +0200

    functions: add more debug info to response

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,12 +1,12 @@
 exports.handler = function (event, context, callback) {
   // Parse data sent in form hook (email, name etc)
-  const { payload } = JSON.parse(event.body)
+  const { data } = JSON.parse(event.body).payload
 
-  console.log('Debug log:\n', payload.data)
+  console.log('Debug log:\n', data)
 
   // success!
   callback(null, {
     statusCode: 200,
-    body: 'mail sent',
+    body: 'mail sent:\n' + JSON.stringify(data),
   })
 }
