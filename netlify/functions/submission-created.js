4f256714c868547cbf50cdbce9e70c38998e26e1
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-05-10 14:14:54 +0200

    fix template to templates

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -28,7 +28,7 @@ exports.handler = function (event, context, callback) {
   const path = require('path')
 
   const buffer = fs.readFileSync(
-    path.join(__dirname, 'template/EmailUser.md'),
+    path.join(__dirname, 'templates/EmailUser.md'),
     { encoding: 'utf-8' }
   )
 
