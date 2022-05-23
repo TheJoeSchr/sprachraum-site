6a668df0560d25f733e441c4950c0418bbb9f3c4
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-05-23 18:45:31 +0200

    fix superflous script tag

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,11 @@
-
 <template lang="pug">
 Layout
-  Landing(:value="{ ...content, translateLink:'/de/'}")
+  Landing(:value="{...content, translateLink: '/de/'}")
 </template>
 <script>
 import Landing from '~/components/Landing.vue'
 import landing from '~/content/it/Index.yaml'
-import { message } from '~/content/it/Interface.yaml'
+import {message} from '~/content/it/Interface.yaml'
 
 export default {
   components: {
@@ -25,4 +24,3 @@ export default {
   },
 }
 </script>
-/script>
