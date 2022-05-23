58eb5482bb64b6b84f79e434b18584da65ef1af7
Joe Schr <8218910+TheJoeSchr@users.noreply.github.com>
2022-05-23 14:52:58 +0200

    JUNIORgoPRO: add new pics for Jobs and Corsi subsections

--- src/pages/it/Junior.vue
+++ src/pages/it/Junior.vue
@@ -1,20 +1,20 @@
 <template lang="pug">
 Layout
   div
-    template(v-for='(section,index) in cards' )
-      MinorHero(:value="{...section, isLeft: (index%2==0)}" )
-        g-image(v-if="0==index", src='~/assets/junior/Bild_2_JuniorinnovativP1250100.JPG' height="300" width="300"  fit="cover")
-        g-image(v-if="1==index", src='~/assets/junior/Bild_3_JuniorJOBS_Francesco_SHF.jpg' height="300" width="300" position="center" fit="cover")
-        g-image(v-if="2==index", src='~/assets/placeholders/pictures/undraw_innovative_re_rr5i.svg' height="300" width="300" position="left" fit="cover")
-        g-image(v-if="3==index", src='~/assets/placeholders/pictures/undraw_sunlight_re_0usx.svg' height="300" width="300" center="top" fit="cover")
-    MinorHero(:value="{...contact, isLeft: true }" v-if="contact")
+    template(v-for='(section, index) in cards' )
+      MinorHero(:value="{...section, isLeft: (index % 2 == 0)}" )
+        g-image(v-if="0 == index", src='~/assets/junior/Bild_2_JuniorinnovativP1250100.JPG' height="300" width="300"  fit="cover" quality=100)
+        g-image(v-if="1 == index", src='~/assets/junior/Bild_3_JuniorJOBS_Francesco_SHF.jpg' height="300" width="300" position="center" fit="cover" quality=100)
+        g-image(v-if="2 == index", src='~/assets/junior/JOBS_juniorgopro.jpg' height="300" width="300" position="left" fit="cover" quality=100)
+        g-image(v-if="3 == index", src='~/assets/junior/CORSI_juniorgopro.jpg' height="300" width="300" fit="cover" quality=100)
+    MinorHero(:value="{...contact, isLeft: true}" v-if="contact")
       g-image.mx-auto.my-4( src='~/assets/images/contact.jpg' height="300" width="300" center="top" fit="cover")
 </template>
 
 <script>
 import MinorHero from '~/components/MinorHero.vue'
 import Layout from '~/layouts/Default.vue'
-import { toBr } from '~/helpers'
+import {toBr} from '~/helpers'
 import content from '~/content/it/Junior.yaml'
 export default {
   name: 'JUNIORgoPRO',
@@ -23,7 +23,7 @@ export default {
     MinorHero,
   },
   data() {
-    return { ...content }
+    return {...content}
   },
   methods: {
     toBr,
